873cefe1c1ec339057a4ac03aa19d0fa5f7584b9
李哲铭 <zhemingli@kugou.net>
2018-12-03 14:55:23 +0800

    使用新的取消“阅读更多”方式

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*
 // 判断“阅读更多”按钮是否存在并模拟点击
 if(exist('#btn-readmore')){
     exist('#btn-readmore').click()
@@ -35,4 +36,35 @@ document.body.onclick=moveRight;
 function moveRight() {
     document.documentElement.scrollLeft=390;
 }
+*/
 
+
+// 查找元素
+function query(css) {
+    return document.querySelector(css);
+}
+
+//删除元素
+function remove(element) {
+    const parentElement = element.parentNode;
+    if (parentElement) {
+        parentElement.removeChild(element);
+    }
+}
+
+const btnReadmore = query('#btn-readmore')
+if (btnReadmore) {
+    console.log('btn-readmore exist');
+
+    // 显示全文和隐藏“阅读更多”按钮
+
+    // csdn页面的设置方式：
+    // $("div.article_content").removeAttr("style");
+    // $('#btn-readmore').parent().remove();
+
+    // 等效替换：
+    query("div.article_content").removeAttribute('style');
+    remove(btnReadmore.parentNode);
+} else {
+    console.log('btn-readmore not exist');
+}
